bd8cb0145738d07c20e455af0b668ad16b0b3da7
missuzundiz <81981945+missuzundiz@users.noreply.github.com>
2021-04-08 19:32:17 +0300

    Create HW2.py

---
+++ Homeworks/HW2.py
@@ -0,0 +1,10 @@
+k_adi = "Tuba"
+sifre ="mormenekse"
+
+k_adi_gir =input("Kullanıcı adınızı giriniz:")
+sifre_gir= input("Parolanızı giriniz:")
+
+if (k_adi == k_adi_gir and sifre == sifre_gir):
+    print("Giriş Başarılı")
+else:
+    print("Giriş Başarısız, Tekrar Deneyin")
